5206e244424d58e770100ffdfdcd2721f7cd2f46
Walle187 <fncreeper138@gmail.com>
2025-12-20 00:42:27 +0100

    ConsoleBudgetTracker

--- learning/src/ConsoleBudgetTracker.java
+++ learning/src/ConsoleBudgetTracker.java
@@ -1,14 +1,31 @@
 import java.util.Scanner;
 
 public class ConsoleBudgetTracker {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String[] incomeSplit;
-
-    Scanner tastatur = new Scanner(System.in);
-    System.out.println("Enter Income: ");
-    tastatur.nextLine();
+        String[] expenseSplit;
+        String input, expenses;
+        int expenseAcc = 0, result = 0;
 
 
-}
+        Scanner tastatur = new Scanner(System.in);
+        System.out.print("Enter Income: ");
+        input = tastatur.nextLine();
+
+        System.out.print("Enter Expenses divided with Commas: ");
+        expenses = tastatur.nextLine();
+        expenses = expenses.replaceAll("[\\s]", "");
+        expenseSplit = expenses.split(",");
+
+        for (int i=0; i< expenseSplit.length; i++) {
+            expenseAcc = expenseAcc + Integer.parseInt(expenseSplit[i]);
+        }
+
+        System.out.println("");
+        result = Integer.parseInt(input) - expenseAcc;
+        System.out.println("Dein Einkommen beträgt: " + input + "€");
+        System.out.println("Deine Ausgaben betragen: " + expenseAcc + "€");
+        System.out.println("Nach allen ausgaben bleiben dir noch " + result + "€");
+        tastatur.close();
+    }
 }
