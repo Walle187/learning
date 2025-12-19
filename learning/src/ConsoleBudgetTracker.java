565edf6f31550d7bbb552b918e78b2cf004d7c5d
Walle187 <fncreeper138@gmail.com>
2025-12-19 22:26:31 +0100

    Initial Setup IntelliJ

---
+++ learning/src/ConsoleBudgetTracker.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConsoleBudgetTracker {
+public static void main(String[] args) {
+
+    String[] incomeSplit;
+
+    Scanner tastatur = new Scanner(System.in);
+    System.out.println("Enter Income: ");
+    tastatur.nextLine();
+
+
+}
+}
